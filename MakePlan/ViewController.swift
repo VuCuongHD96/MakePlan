f2ef94518999afbc7f16525156eb203fc82a3b41
Sun <sunsetno08@gmail.com>
2022-04-16 12:00:53 +0700

    Initial Commit

---
+++ MakePlan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakePlan
+//
+//  Created by Sun on 4/16/22.
+//  Copyright © 2022 Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
